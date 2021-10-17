efc58c7c9a8addc0f79c813940dc501153bab90d
qhk1 <3290173703@qq.com>
2021-10-17 14:17:13 +0800

    组件分割

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,27 +5,7 @@
     </div>
     <div class="element-body">
       <div class="element-body-left">
-        <el-row class="tac">
-          <el-col :span="12">
-            <el-menu
-              default-active="2"
-              class="el-menu-vertical-demo"
-              @open="handleOpen"
-              @close="handleClose"
-            >
-              <el-menu-item index="1">
-                <i class="el-icon-menu"></i>
-                <router-link to="/nav1">导航一</router-link>
-                <!-- <span slot="title">导航一</span> -->
-              </el-menu-item>
-              <el-menu-item index="2">
-                <i class="el-icon-menu"></i>
-                <!-- <span slot="title">导航二</span> -->
-                <router-link to="/nav2">导航二</router-link>
-              </el-menu-item>
-            </el-menu>
-          </el-col>
-        </el-row>
+        <Nav></Nav>
       </div>
       <div class="element-body-right"><router-view></router-view></div>
     </div>
@@ -35,15 +15,10 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
-
+import Nav from "./nav";
 export default {
-  methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
-    },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
-    },
+  components: {
+    Nav,
   },
 };
 </script>
---
+++ src/views/nav.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <el-row class="tac">
+      <el-col :span="12">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+        >
+          <el-menu-item index="1">
+            <i class="el-icon-menu"></i>
+            <router-link to="/nav1">导航一</router-link>
+            <!-- <span slot="title">导航一</span> -->
+          </el-menu-item>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <!-- <span slot="title">导航二</span> -->
+            <router-link to="/nav2">导航二</router-link>
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
